2a45115b2de2de754f00ac37ffce78156242239b
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-15 18:39:37 -0300

    exercicio8

---
+++ exercicio8/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * 8. Faça um programa em VS que solicite um numero inteiro. S
+ * e o numero não for inteiro, solicite-o até que seja. após, 
+ * informe se ele é par ou impar. 
+ * Finalmente, pergunte ao usuário se ele quer repetir o programa em VS.
+ */
+
+string desejaContinuar = "";
+
+do
+{
+    int numeroInteiro;
+
+    Console.Write("Digite um numero inteiro: ");
+
+    string numeroDigitado = Console.ReadLine();
+
+    while (!(int.TryParse(numeroDigitado, out numeroInteiro)
+            && numeroInteiro % 1 == 0))
+    {
+        Console.WriteLine("Numero invalido, digite novamente.");
+        numeroDigitado = Console.ReadLine();
+
+    }
+
+    if (numeroInteiro % 2 == 0)
+    {
+        Console.WriteLine(numeroInteiro+ " é par");
+    }
+    else
+    {
+        Console.WriteLine(numeroInteiro + " é impar");
+    }
+
+    while (desejaContinuar != "S" && desejaContinuar != "N")
+    {
+        Console.WriteLine("Você so pode digitar S ou N, digite novamente: ");
+        desejaContinuar = Console.ReadLine().ToUpper();
+
+    }
+
+} while (desejaContinuar == "S");
