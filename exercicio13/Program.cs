b167b04f3f114100708d7b79d92c03feb06d9f0a
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-17 20:36:18 -0300

    exercicio13

---
+++ exercicio13/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Elaborar um programa que apresente o somatório dos valores pares existentes 
+entre 1 e 500.
+ */
+
+int n1 = 0;
+int i;
+
+for (i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        n1 = n1 + i;
+    }
+
+}
+
+Console.WriteLine("A soma dos números de 1 a 500 é: " + n1);
