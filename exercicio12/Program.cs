50ba798b510ba883e9fa1a9d3a9db9ed503958e6
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-17 20:26:01 -0300

    exercicio12

---
+++ exercicio12/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * 12. Apresentar o total da soma dos cem primeiros números inteiros.
+ */
+
+int i, soma = 0;
+
+
+for (i = 0; i <= 100; i++)
+{
+    soma = soma + i;
+    
+
+}
+Console.WriteLine("O total da soma é: " + soma);
