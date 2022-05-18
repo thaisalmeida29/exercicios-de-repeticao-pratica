b2228fb6cee424960d27c1c49b287f52e4ffae47
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-17 21:18:25 -0300

    exercicio14

---
+++ exercicio14/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * 14. Mostrar as potências de 2 variando de 0 a 10.
+ */
+
+int poten = 2 ;
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+
+    Console.WriteLine($"{poten}^{i} = {(long)Math.Pow(poten, i)}");
+}
+   
